500afd8800100af73fe0e6d439ce1da5b7f2eeaf
Mike Goodwin <goodo73@gmail.com>
2015-06-18 03:13:09 +1000

    Added functionality for the projects section

---
+++ source/javascripts/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	var $details = $('#project-details');
+
+	$('.card').on('click',function() {
+		// Get the name of the clicked project card
+		var name = $(this).data('name');
+
+		// Target the corresponding project detail element
+		var detailEl = $details.find('div[data-name=' + name + ']')[0];
+
+		// Display the targetted element, and hide all the others, OR
+		// Just hide the already displayed targetted element
+		if ($(detailEl).is(':hidden')) {  
+			$details.children().hide();
+		}
+
+		$(detailEl).slideToggle();
+	})
+})
